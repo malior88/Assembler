f186313c309957bdd44b1929876fc28b84c675eb
malior88 <143396363+malior88@users.noreply.github.com>
2023-09-24 13:04:57 +0300

    Create Main.h

---
+++ Main.h
@@ -0,0 +1,40 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+
+#include <limits.h>
+
+#include <stddef.h>
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <string.h>
+
+#include "Pre_assembler.h"
+
+#include "SymbolTable.h"
+
+#include "Decode.h"
+
+#include "FirstPass.h"
+
+#include "SecondPass.h"
+
+
+
+
+/* Function prototypes for main.c */
+
+int main(int argc, char* argv[]);
+
+
+#endif /* MAIN_H */
+
+
+
+
+
